9d0e968364b192221a7303d499840e5f15c7eb87
DevExpressExampleBot <fakeEmail>
2022-06-23 23:04:37 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,24 +1,28 @@
-﻿Imports System
 Imports DevExpress.Xpf.Core
 
 Namespace DXSample
-	Partial Public Class MainWindow
-		Inherits DXWindow
 
-		Private Const LayoutFileName As String = "layout.xml"
-		Private Const WorkspaceName As String = "workspace1"
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub Button_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			Dim manager As IWorkspaceManager = WorkspaceManager.GetWorkspaceManager(dockLayoutManager)
-			manager.CaptureWorkspace(WorkspaceName)
-			manager.SaveWorkspace(WorkspaceName, LayoutFileName)
-		End Sub
-		Private Sub Button_Click_1(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			Dim manager As IWorkspaceManager = WorkspaceManager.GetWorkspaceManager(dockLayoutManager)
-			manager.LoadWorkspace(WorkspaceName, LayoutFileName)
-			manager.ApplyWorkspace(WorkspaceName)
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits DXWindow
+
+        Const LayoutFileName As String = "layout.xml"
+
+        Const WorkspaceName As String = "workspace1"
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub Button_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim manager As IWorkspaceManager = WorkspaceManager.GetWorkspaceManager(Me.dockLayoutManager)
+            manager.CaptureWorkspace(WorkspaceName)
+            manager.SaveWorkspace(WorkspaceName, LayoutFileName)
+        End Sub
+
+        Private Sub Button_Click_1(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Dim manager As IWorkspaceManager = WorkspaceManager.GetWorkspaceManager(Me.dockLayoutManager)
+            manager.LoadWorkspace(WorkspaceName, LayoutFileName)
+            manager.ApplyWorkspace(WorkspaceName)
+        End Sub
+    End Class
 End Namespace
