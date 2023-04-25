667692c570662d674e5e3fbbc94eefa79f0aae66
DevExpressExampleBot <fakeEmail>
2023-04-25 11:28:06 +0400

    Source auto update [skip ci]

--- VB/DX WPF Application10/MainWindow.xaml.vb
+++ VB/DX WPF Application10/MainWindow.xaml.vb
@@ -1,40 +1,28 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.Xpf.Core
 Imports DevExpress.Xpf.Core.Serialization
 
 Namespace DXWpfApplication
-	Partial Public Class MainWindow
-		Inherits DXWindow
-		Public Sub New()
-			InitializeComponent()
 
-		End Sub
-		Private Sub ButtonSave_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			DXSerializer.Serialize(Me, "layout.xml", "DXWpfApplication", New DXOptionsLayout() With {.AcceptNestedObjects = AcceptNestedObjects.VisualTreeOnly})
-		End Sub
-		Private Sub ButtonRestore_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
-			DXSerializer.Deserialize(Me, "layout.xml", "DXWpfApplication", New DXOptionsLayout() With {.AcceptNestedObjects = AcceptNestedObjects.VisualTreeOnly})
-		End Sub
-	End Class
-	Friend Class TestData
-		Private privateText As String
-		Public Property Text() As String
-			Get
-				Return privateText
-			End Get
-			Set(ByVal value As String)
-				privateText = value
-			End Set
-		End Property
-		Private privateNumber As Integer
-		Public Property Number() As Integer
-			Get
-				Return privateNumber
-			End Get
-			Set(ByVal value As Integer)
-				privateNumber = value
-			End Set
-		End Property
-	End Class
+    Public Partial Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub ButtonSave_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Call DXSerializer.Serialize(Me, "layout.xml", "DXWpfApplication", New DXOptionsLayout() With {.AcceptNestedObjects = AcceptNestedObjects.VisualTreeOnly})
+        End Sub
+
+        Private Sub ButtonRestore_Click(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs)
+            Call DXSerializer.Deserialize(Me, "layout.xml", "DXWpfApplication", New DXOptionsLayout() With {.AcceptNestedObjects = AcceptNestedObjects.VisualTreeOnly})
+        End Sub
+    End Class
+
+    Friend Class TestData
+
+        Public Property Text As String
+
+        Public Property Number As Integer
+    End Class
 End Namespace
